a59664cdb69f56df689955d33523ad28d7e90ebf
Jendospendos <138603556+Jendospendos@users.noreply.github.com>
2023-12-18 17:23:04 +0300

    Add files via upload

---
+++ prog/utils/initialCards.js
@@ -0,0 +1,25 @@
+const initialCards = [
+  {
+    name: 'Prelude V',
+    link: 'https://hdpic.club/uploads/posts/2022-01/1641981394_3-hdpic-club-p-khonda-prelyud-1997-3.jpg'
+  },
+  {
+    name: 'Prelude IV',
+    link: 'https://i.pinimg.com/originals/e9/e4/70/e9e470be5c7ce6fe479a05334fddf1d8.jpg'
+  },
+  {
+    name: 'Prelude III',
+    link: 'https://oldmotors.net/wp-content/uploads/2020/11/AK_RedPrelude_IG.jpg'
+  },
+  {
+    name: 'Prelude II',
+    link: 'https://a.d-cd.net/wQAAAgPBieA-1920.jpg'
+  },
+  {
+    name: 'Prelude I',
+    link: 'https://avatars.dzeninfra.ru/get-zen_doc/4782316/pub_62a8f02b8f4f1a4e3a97caf5_62a8f03686dde05b8739e62c/scale_1200'
+  },
+  
+];
+
+export default initialCards;
